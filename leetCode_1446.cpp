698c9fe46f031e114c55435846e3e7ff642fe8a4
ZLinTsai <37646878+ZlinTsai@users.noreply.github.com>
2021-12-13 19:57:10 +0800

    add solution

--- leetCode_1446.cpp
+++ leetCode_1446.cpp
@@ -4,14 +4,38 @@
 
 #include <iostream>
 #include <vector>
+#include <algorithm>
 
 using namespace std;
 
 class Solution {
 public:
     int maxPower(string s) {
+
+        int iMax = 0;
+        int iCount = 1;
+        char prevChar = s[0];
         
-        return 0;
+        for (size_t i = 1; i < s.size(); i++)
+        {
+            //cout << s[i] << ", " <<iCount<< endl;
+            if (prevChar == s[i])
+            {
+                iCount++;
+            }
+            else
+            {
+                cout << "?? " << iCount <<endl;
+                iMax = max(iMax, iCount);
+                prevChar = s[i];
+                iCount = 1;
+            }
+        }
+
+        // for last one
+        iMax = max(iMax, iCount);
+
+        return iMax;
     }
 };
 
@@ -20,7 +44,7 @@ int main()
     Solution sol;
     string s = "leetcode";
 
-    bool ans = sol.maxPower(s);
+    int ans = sol.maxPower(s);
 
     cout << ans << endl;
 
