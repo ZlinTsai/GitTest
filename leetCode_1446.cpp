875198d80a802aae96cbdd2c67136891e84fb387
ZLinTsai <37646878+ZlinTsai@users.noreply.github.com>
2021-12-13 19:41:12 +0800

    initial

---
+++ leetCode_1446.cpp
@@ -0,0 +1,28 @@
+// Quest: 1446. Consecutive Characters
+// Level: Easy
+// Data:  2021/12/13
+
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+class Solution {
+public:
+    int maxPower(string s) {
+        
+        return 0;
+    }
+};
+
+int main()
+{
+    Solution sol;
+    string s = "leetcode";
+
+    bool ans = sol.maxPower(s);
+
+    cout << ans << endl;
+
+    return 0;
+}
